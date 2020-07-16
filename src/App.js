7d00fd2c294c3d18722c28a4ac18e7087e3044e8
SpencerBlum <spencerblum@gmail.com>
2020-07-16 15:49:43 -0400

    fixed white space error with react router

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import {  Switch, Route, Redirect } from 'react-router-dom';
 import  Home from "./Routes/Home.js"
 
 
 
 function App() {
   return (
-    <div >
-      <Router >
-        <Route  exact path="/Home"   component={Home} />
+      <Switch>
+        <Route  exact path="/Home" component={Home} />
         <Route  exact path="*"  render={() => <Redirect to ="/Home" /> } />
-      </Router>
-    </div>
+     </Switch>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,8 @@ const Header = () => {
 
 return(
     <div className= "header">
-         <img classname= "logo" src={require("../images/SpencerBlumlogo.png")} height = "20px"/>
+        <img classname= "logo" src={require("../images/SpencerBlumlogo.png")} height = "20px"/>
+        {/* <a href="https://drive.google.com/file/d/1cCwsRbW-ieGuPwFU1GbLeaSRWas9CghR/view?usp=sharing"><button>Resume</button></a> */}
     </div>
 )
  }
